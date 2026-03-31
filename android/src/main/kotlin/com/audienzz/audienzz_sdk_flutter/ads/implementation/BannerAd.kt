a1207b7cf92cd7e5f4bb2ab779ad5c2cdd787a47
elysiak <yevhenii.lysiak@audienzz.com>
2026-03-31 16:36:53 +0300

    Implement smart refresh support.

--- android/src/main/kotlin/com/audienzz/audienzz_sdk_flutter/ads/implementation/BannerAd.kt
+++ android/src/main/kotlin/com/audienzz/audienzz_sdk_flutter/ads/implementation/BannerAd.kt
@@ -25,6 +25,8 @@ class BannerAd(
     private val auConfigId: String,
     private val adSizes: List<AdSize>,
     private val isAdaptiveSize: Boolean,
+    private val isLazyLoad: Boolean,
+    private val smartRefresh: Boolean,
     private val refreshTimeInterval: Int?,
     private val adFormat: AdFormat,
     private val apiParameters: List<AudienzzSignals.Api>,
@@ -98,8 +100,12 @@ class BannerAd(
         }
 
         currentAdView?.let { adView ->
-            AudienzzAdViewHandler(adView, adUnit).load { request, _ ->
+            val handler = AudienzzAdViewHandler(adView, adUnit)
+            handler.load(withLazyLoading = isLazyLoad) { request, _ ->
                 adView.loadAd(request)
+            }
+            if (smartRefresh) {
+                handler.enableSmartRefresh()
             }
         }
     }
